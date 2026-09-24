986775afcde2d32610e0095bccd05936641329dd
Voadmined <voadmined@gmail.com>
2026-09-24 23:07:56 +0200

    Initial Release
    Version 1.0.0

---
+++ pass-gen.py
@@ -0,0 +1,48 @@
+# Version 1.0
+# Password Generator
+
+import random
+
+characters = [
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+    "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+]
+
+numbers = [
+    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+]
+
+symbols = [
+    "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".",
+    "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`",
+    "{", "|", "}", "~"
+]
+
+passLenth = int(input("Password lenght: "))
+
+passwordArray = []
+
+for i in range(50):
+    for i in range(passLenth):
+
+        varType = random.randint(1, 3)
+
+        if varType == 1:
+            charArr = characters.__len__
+            slctChar = random.randint(0, len(characters)-1)
+            passwordArray.append(characters[slctChar])
+        
+        elif varType == 2:
+            slctNum = random.randint(0, len(numbers)-1)
+            passwordArray.append(numbers[slctNum])
+
+        elif varType == 3:
+            slctSym = random.randint(0, len(symbols)-1)
+            passwordArray.append(symbols[slctSym])
+
+    password = "".join(passwordArray)
+    print(password)
+    passwordArray = []
